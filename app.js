9c535fe5eb60f4a09846e2fd03ebc5f73d3e0c58
miguel <miguel.espinoza.silloca@gmail.com>
2021-03-22 17:58:08 -0500

    Feature: * Adding a port to listen

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const dotenv = require('dotenv')
 dotenv.config({path: './config/config.env'})
 
 const app = express()
+
+//
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log('connected')
+})
