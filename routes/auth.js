1d0e3493f17d9813d8cc3e7979bd10c683d2710b
miguel <miguel.espinoza.silloca@gmail.com>
2021-03-24 10:26:50 -0500

    Feature: * Creating routes for authentication

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,17 @@ const authController = require('../controllers/auth')
 const router = express.Router()
 
 router.post('/register', authController.register)
+
 router.post('/login', authController.login)
+
 router.get('/logout', authController.logout)
+
+router.get('/me', authController.getMe)
+
+router.put('/updatedetails', authController.updateDetails)
+
+router.post('/forgotpassword', authController.updateDetails)
+
+router.post('/resetpassword/:resettoken', authController.updateDetails)
 
 module.exports = router
